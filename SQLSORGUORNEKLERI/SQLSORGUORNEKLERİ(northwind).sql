3236f10736e582a56f3c4100aa17979ce5f17c5d
FulyaSarıyar <fulyasariyarabdullah@outlook.com>
2021-11-27 15:29:01 +0300

    SorguOrnekler.

---
+++ SQLSORGUORNEKLERI/SQLSORGUORNEKLERİ(northwind).sql
@@ -0,0 +1,14 @@
+--SQL Sorgu Ornekleri 
+--Customers tablosunda bulunan tum sutunlari listeleyiniz.
+use Northwind
+
+go
+
+SELECT *
+FROM Customers
+
+--Customers tablosunda bulunan ContactName ve City sutunlarini listeleyiniz.
+
+SELECT ContactName, City
+FROM Customers
+
