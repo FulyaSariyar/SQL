94d09a3e247c16f83116809ea75e2101c80dcb90
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2021-11-29 09:39:05 +0300

    StoredProcedures.

---
+++ StoredProcedures.sql
@@ -0,0 +1,26 @@
+--TANE PARAMETRE ALACAK. 1 KATEGORI ADI, 2 URUNADI,3 FIYATI
+--EGER GIRILEN KATEGORIDE BIR KATEGORI YOKSA EKLESIN VARSA O KATEGORI IDSI ILE URUN EKLESIN (FIYAT)
+
+
+ALTER PROCEDURE SP_KATEGORI_URUN 
+@CategoryName nvarchar(15),
+@ProductName nvarchar(40),
+@UnitPrice money
+AS
+BEGIN
+    DECLARE @CatId int
+	SELECT @CatId =CategoryID FROM Categories WHERE CategoryName =@CategoryName
+	IF @CatId IS NULL
+	BEGIN
+				INSERT INTO Categories(CategoryName) VALUES (@CategoryName)
+				INSERT INTO Products(ProductName,UnitPrice,CategoryID) VALUES 
+				(@ProductName,@UnitPrice,@@identity)
+	END
+    ELSE 
+	INSERT INTO Products(ProductName,UnitPrice,CategoryID) VALUES 
+				(@ProductName,@UnitPrice,@CatId)
+	PRINT(CAST(@@identity AS NVARCHAR(150))+'idli urun eklenmistir.')
+  
+END
+
+EXEC SP_KATEGORI_URUN 'Beverages','Ayran',5
