88dccb14b1a6cd55ffff58fbaf7320aba690db29
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2021-11-29 10:59:34 +0300

    SqlTry-catch.

--- GUN5/Transaction.sql
+++ GUN5/Transaction.sql
@@ -17,19 +17,40 @@ insert into Hesaplar values ('Berna',7500)
 insert into Hesaplar values ('Gizem',1500)
 
 
-create proc sp_havale_yap
+alter proc sp_havale_yap
 @aliciId int,
 @gondericiId int,
 @tutar decimal(12,3)
 as
 begin
-	update Hesaplar set Bakiye-= @tutar where Id = @gondericiId
-	update Hesaplar set Bakiye+= @tutar where Id = @aliciId
+	BEGIN TRY
+	BEGIN TRAN HavaleTran
+	if exists (select * from Hesaplar where Id=@gondericiId)
+		begin 
+			update Hesaplar set Bakiye-= @tutar where Id = @gondericiId
+		end
+	else 
+		begin
+			raiserror('Gonderici bulunamadi.',16,1)
+		end
+	if exists (select * from Hesaplar where Id=@aliciId)
+		begin 
+			update Hesaplar set Bakiye-= @tutar where Id = @aliciId
+		end
+	else
+	   begin
+	        raiserror('Alici bulunamadi.',16,1)
+		end
+	COMMIT TRAN HavaleTran
+		END TRY
+		BEGIN CATCH
+			print (ERROR_MESSAGE())
+		END CATCH
 end
-
 
 select * from Hesaplar
 
 exec sp_havale_yap 2,3,1500
 exec sp_havale_yap 23,1,5000
 exec sp_havale_yap 1,23,5000000
+
